477f60f3375696c98fa15d95060d81e90902808c
confusedpotatoe <milo.arvaa@gmail.com>
2026-06-04 14:59:37 +0200

    added repositories for generic and product

---
+++ FullstackTest.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using FullstackTest.Domain.Entities;
+
+namespace FullstackTest.Application.Interfaces
+{
+	public interface IProductRepository : IGenericRepository<Product>
+	{
+		Task<IEnumerable<Product>> GetProductsByCategoryIdAsync(int categoryId);
+		Task<IEnumerable<Product>> GetProductsByIdAsync(IEnumerable<int> productIds);
+		Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+
+	}
+}
--- FullstackTest.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FullstackTest.Infrastructure
-{
-	public class Class1
-	{
-
-	}
-}
