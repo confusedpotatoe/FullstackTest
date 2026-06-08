8cfde5fbab3556d54ff4b9d3740ebe01c3b70b27
confusedpotatoe <milo.arvaa@gmail.com>
2026-06-08 12:18:10 +0200

    added castogory repositiry

---
+++ FullstackTest.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using FullstackTest.Application.Interfaces;
+using FullstackTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullstackTest.Infrastructure.Repositories
+{
+	public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+	{
+		public CategoryRepository(AppDbContext context) : base(context)
+		{
+		}
+
+		public async Task<IEnumerable<Category>> GetCategoriesWithProductsAsync()
+		{
+			return await _context.Categories
+				.Include(c => c.Products)
+				.ToListAsync();
+		}
+	}
+}
