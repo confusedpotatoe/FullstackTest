ec9c58feee50f6c853702b970abaaf035e2933e7
confusedpotatoe <milo.arvaa@gmail.com>
2026-06-08 15:00:32 +0200

    added controller for product and category

---
+++ FullstackTest.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using FullstackTest.Application.DTOs;
+using FullstackTest.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullstackTest.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ICategoryService _categoryService;
+		public CategoriesController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAll()
+		{
+			var categories = await _categoryService.GetAllCategoriesAsync();
+			return Ok(categories);
+		}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<CategoryDto>> GetById(int id)
+		{
+			var category = await _categoryService.GetCategoryByIdAsync(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return Ok(category);
+		}
+		[HttpPost]
+		public async Task<ActionResult> Create(CreateCategoryDto createDto)
+		{
+			await _categoryService.CreateCategoryAsync(createDto);
+			return Ok();
+		}
+		[HttpPut("{id}")]
+		public async Task<ActionResult> Update(int id, UpdateCategoryDto updateDto)
+		{
+			await _categoryService.UpdateCategoryAsync(id, updateDto);
+			return Ok();
+		}
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			await _categoryService.DeleteCategoryAsync(id);
+			return Ok();
+		}
+	}
+}
---
+++ FullstackTest.Api/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using FullstackTest.Application.DTOs;
+using FullstackTest.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullstackTest.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+
+	public class ProductsController : ControllerBase
+	{
+		private readonly IProductService _productService;
+		public ProductsController(IProductService productService)
+		{
+			_productService = productService;
+		}
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+		{
+			var products = await _productService.GetAllProductsAsync();
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ProductDto>> GetById(int id)
+		{
+			var product = await _productService.GetProductByIdAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return Ok(product);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Create(CreateProductDto createDto)
+		{
+			await _productService.CreateProductAsync(createDto);
+			return Ok();
+		}
+
+		[HttpPut("{id}")]
+		public async Task<ActionResult> Update(int id, UpdateProductDto updateDto)
+		{
+			await _productService.UpdateProductAsync(id, updateDto);
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			await _productService.DeleteProductAsync(id);
+			return Ok();
+		}
+	}
+}
